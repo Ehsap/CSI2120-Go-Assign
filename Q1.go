5654af079ecd353d43b513c997511a1ae6d5cc74
Justin Huynh <polar.truth@gmail.com>
2018-04-06 14:21:55 -0400

    Final

--- Q1.go
+++ Q1.go
@@ -12,22 +12,72 @@ import (
 	"os"
 )
 
-func AbsDiff(sliceA, sliceB []float32) (res []float32, err error) {
-	//Confirm that slice lengths are the same
-	if len(sliceA) != len(sliceB){
-		err = errors.New("Slices are not the same length")
-		return nil, err
+func AbsDiff(sliceA, sliceB []float32, version int) (res []float32, err error) {
+
+	if version == 0{
+		//Confirm that slice lengths are the same
+		if len(sliceA) != len(sliceB){
+			err = errors.New("Slices are not the same length")
+			return nil, err
+		}
+		
+		//Find the absolute difference
+		for i:= 0; i < len(sliceA); i++ {
+			num := float32(math.Abs(float64(sliceA[i]) - float64(sliceB[i])))
+			res = append(res, num)
+		}	
+		return res, nil
+	
+	}else if version == -1 {
+		//Find the shorter slice
+		if len(sliceA) < len(sliceB){ //SliceA is shorter 
+			//Add 0's to slice A so that both slices are same length
+			for i:= len(sliceA); i < (len(sliceA) + len(sliceB) - len(sliceA)); i++{
+				sliceA = append(sliceA, 0)
+			}
+			
+			//Find the absolute difference
+			for i:=0; i < len(sliceB); i++{
+				num := float32(math.Abs(float64(sliceA[i]) - float64(sliceB[i])))
+				res = append(res, num)
+			}
+		return res, nil
+		
+		}else { //SliceB is shorter
+			//Add 0's to slice B so that both slices are same length
+			for i:= len(sliceB); i < (len(sliceB) + len(sliceA) - len(sliceB)); i++{
+				sliceB = append(sliceB, 0)
+			}
+			
+			//Find the absolute difference
+			for i:=0; i < len(sliceA); i++{
+				num := float32(math.Abs(float64(sliceA[i]) - float64(sliceB[i])))
+				res = append(res, num)
+			}
+		return res, nil
+		}
+		
+	}else if version == 1 {
+		//Find the shorter slice
+		var lengthShortest int
+		
+		if len(sliceA) < len(sliceB){
+			lengthShortest = len(sliceA)
+		} else {
+			lengthShortest = len(sliceB)
+		}
+		//Find the absolute difference
+		for i:=0; i < lengthShortest; i++{
+			num := float32(math.Abs(float64(sliceA[i]) - float64(sliceB[i])))
+			res = append(res, num)
+		}
+		return res, nil	
 	}
 	
-	//Find the absolute difference
-	for i:= 0; i < len(sliceA); i++ {
-		num := float32(math.Abs(float64(sliceA[i]) - float64(sliceB[i])))
-		res = append(res, num)
-	}
-	
-	
-	return res, nil
-	}
+	return nil, err
+
+}
+
 
 
 func main() {
@@ -37,7 +87,12 @@ func main() {
         sliceA []float32
 		sliceB []float32
 		cont string
+		version int
     )
+	
+	//Ask for the version
+	fmt.Println("Enter the version. [-1, 0, 1]")
+	fmt.Scanf("%d\n", &version)
 	
 	//Get the first slice
 	fmt.Println("Please enter a slice. (Press enter to end slice)")
@@ -63,12 +118,12 @@ func main() {
 			sliceB = append(sliceB, next)
 		}
 		
-		res, err := AbsDiff(sliceA, sliceB)
+		res, err := AbsDiff(sliceA, sliceB, version)
 		
 		if err == nil && res != nil{
 			fmt.Printf("Result: ")
 			fmt.Printf("%v", res)
-			fmt.Println("q to quit (Anything else to continue):")
+			fmt.Println("q to quit (Enter to continue):")
 			fmt.Scanf("%v", &cont)
 			sliceA = sliceB
 			
@@ -78,7 +133,7 @@ func main() {
 		} else {
 			fmt.Println(err)
 			//fmt.Println("Slices are not the same length")
-			fmt.Println("q to quit (Anything else to continue):")
+			fmt.Println("q to quit (Enter to continue):")
 			fmt.Scanf("%v", &cont)
 			
 			if cont == "q" {
